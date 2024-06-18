c0488786e61eabc16126c8e8e91119748ad3d4e7
Muskan Swarup <muskanswarup680@gmail.com>
2024-06-18 20:50:57 +0530

    added edit functionality

--- index.js
+++ index.js
@@ -19,7 +19,17 @@ app.get("/" , function(req, res){
 app.get('/files/:filename' , function(req, res){
     fs.readFile(`./files/${req.params.filename}` , "utf-8" , function(err, filedata){
         res.render('show' , {filename: req.params.filename, filedata: filedata});
-        // console.log(filedata);
+    })
+})
+
+// Edit
+app.get('/edit/:filename' , function(req, res){
+    res.render('edit', {filename: req.params.filename});
+})
+
+app.post('/edit', function(req, res){
+    fs.rename(`./files/${req.body.previous}`, `./files/${req.body.new}` , function(err){
+        res.redirect("/");
     })
 })
 
